4d2f2c4ca9ae54405af9cebc181130f1640e56f8
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2014-02-08 15:33:20 -0200

    Pending changes in common.h and stylistic changes

--- src/cksum.c
+++ src/cksum.c
@@ -23,58 +23,58 @@
 /* This function is 5 times faster than the "official" rfc 1071 implementation (and shortter too!). */
 uint16_t cksum(void *data, size_t length)
 {
-	uint64_t sum, *p = data;
-	uint32_t t1, t2;
+  uint64_t sum, *p = data;
+  uint32_t t1, t2;
   uint16_t t3, t4;
-	
-	sum = 0;
+  
+  sum = 0;
 
-	/* Sums 8 bytes at a time... */
+  /* Sums 8 bytes at a time... */
   while (length >= sizeof(uint64_t))
-	{
-	  uint64_t s = *p++;
-		sum += s;
+  {
+    uint64_t s = *p++;
+    sum += s;
     if (sum < s) sum++;
-		length -= sizeof(uint64_t);
-	}
+    length -= sizeof(uint64_t);
+  }
 
-	/* Sums the remaing data, if any */
-	data = p;
-	if (length >= sizeof(uint32_t))
-	{
+  /* Sums the remaing data, if any */
+  data = p;
+  if (length >= sizeof(uint32_t))
+  {
     uint32_t s = *(uint32_t *)data;
-		sum += s;
+    sum += s;
     if (sum < s) sum++;
-		length -= sizeof(uint32_t);
-		data += sizeof(uint32_t);
-	}
+    length -= sizeof(uint32_t);
+    data += sizeof(uint32_t);
+  }
 
-	if (length >= sizeof(uint16_t))
-	{
-		uint16_t s = *(uint16_t *)data;
-		sum += s;
-		if (sum < s) sum++;
-		length -= sizeof(uint16_t);
-		data += sizeof(uint16_t);
-	}
+  if (length >= sizeof(uint16_t))
+  {
+    uint16_t s = *(uint16_t *)data;
+    sum += s;
+    if (sum < s) sum++;
+    length -= sizeof(uint16_t);
+    data += sizeof(uint16_t);
+  }
 
-	if (length)
-	{
-		uint8_t s = *(uint8_t *)data;
-		sum += s;
-		if (sum < s) sum++;
-	}
+  if (length)
+  {
+    uint8_t s = *(uint8_t *)data;
+    sum += s;
+    if (sum < s) sum++;
+  }
 
-	/* Fold down to 16 bits */
-	t1 = sum;
-	t2 = sum >> 32;
-	t1 += t2;
-	if (t1 < t2) t1++;
+  /* Fold down to 16 bits */
+  t1 = sum;
+  t2 = sum >> 32;
+  t1 += t2;
+  if (t1 < t2) t1++;
 
-	t3 = t1;
-	t4 = t1 >> 16;
-	t3 += t4;
-	if (t3 < t4) t3++;
+  t3 = t1;
+  t4 = t1 >> 16;
+  t3 += t4;
+  if (t3 < t4) t3++;
 
-	return ~t3;
+  return ~t3;
 }
