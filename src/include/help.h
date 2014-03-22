4fdee6995625836cc289dd8db0d633a3943a9386
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2014-03-22 17:26:33 -0300

    Added instructions comment

--- src/include/help.h
+++ src/include/help.h
@@ -1,6 +1,9 @@
 #ifndef __HELP_INCLUDED__
 #define __HELP_INCLUDED__
 
+/* Add usage function interface here.
+   Add usage function definition for protocol at src/help/ directory.
+   Change Makefile and src/usage.c. */
 extern void general_help(void);
 extern void gre_help(void);
 extern void tcp_udp_dccp_help(void);
